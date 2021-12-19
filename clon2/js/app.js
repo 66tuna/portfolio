068a02b3bb6e6ee15abcf8eb1bf273947768fb71
66tuna <66tuna1018@gmail.com>
2021-12-20 00:35:16 +0900

    s1220

--- clon2/js/app.js
+++ clon2/js/app.js
@@ -7,5 +7,22 @@ function slidr(){
     }
     // let liW = $('#box01 ul>li').innerWidth();
     // $('#box01 ul').css('marginLeft', `-${liW*2}px`);
+}
 
+function navBtn(){
+    $('#btn').on('click', function(){
+        $(this).hide();
+        $('.back').show();
+        $('#headerWrap nav').animate({left: 0}, 400);
+        $('#headerWrap .close').show();
+    })
+
+    $('#headerWrap .close').on('click', function(){
+        let navW = $(this).parents().innerWidth();
+        $('#headerWrap nav').animate({left: `-${navW}px`}, 400, function(){
+            $('#btn').show();
+        });
+        $(this).hide();
+        $('.back').hide();
+    })
 }
