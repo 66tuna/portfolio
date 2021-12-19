41b234920ab5c3e0c3eecbf2495ee2ab18633724
66tuna <66tuna1018@gmail.com>
2021-12-19 19:05:49 +0900

    s1219

--- clon2/js/app.js
+++ clon2/js/app.js
@@ -1,11 +1,11 @@
 function slidr(){
     for(let i=0; i<3; i++){
         $('#box01 ul>li').eq(i).css({
-            backgroundImage:  `url('../images/slid${i+1}.jpg')`,
+            backgroundImage:  `url('./images/slid${i+1}.jpg')`,
             
         });
     }
-    let liW = $('#box01 ul>li').innerWidth();
-    $('#box01 ul').css('marginLeft', `-${liW*2}px`);
+    // let liW = $('#box01 ul>li').innerWidth();
+    // $('#box01 ul').css('marginLeft', `-${liW*2}px`);
 
 }
